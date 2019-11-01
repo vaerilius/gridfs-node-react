6dc351644035d045656148288060fc095417a76b
vaerilius <tjohannest@gmail.com>
2019-11-01 13:09:09 +0200

    frondend work

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
+const config = require('./utils/config')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+  console.log('connected to MingoDB')
+})
+.catch((err) => {
+  console.log(error.message)
+})
 
 app.use(bodyParser.json())
 
 app.get('/', async(request, response) => {
-  const data = await 
+  // const data = await 
 
-  response.json(data.map(d  => d.toJ)))
+  // response.json(data.map(d  => d.toJ)))
 })
 
 module.exports = app
--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,28 +1,49 @@
-import React from 'react'
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import { Button, TextField } from '@material-ui/core';
+import React, { useState } from 'react'
+import { DropzoneArea } from 'material-ui-dropzone'
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import axios from 'axios'
 
 const App = () => {
 
-  return(
-    <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="sm">
-      <Button variant="contained">
-        Default
-      </Button>
-      <input
-        accept="image/*"
-       
-        id="contained-button-file"
-        multiple
-        type="file"
-      />
-      </Container>
-    </React.Fragment>
-  )
-}
+  const [files, setfiles] = useState([])
 
+
+  const handleChange = (data) => {
+    console.log(data)
+    setfiles([...files, data])
+  }
+  const handleClick = async () => {
+    console.log(...files)
+   const response = await axios.post('http://localhost:5000/', [...files])
+  }
+
+
+  return (
+    <>
+      <DropzoneArea
+        onChange={ handleChange.bind(this)}
+      />
+      <label htmlFor="contained-button-file">
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6}>
+            <Grid container spacing={1} direction="column" alignItems="center">
+              <Grid item>
+                <ButtonGroup size="small" aria-label="huge outlined button group">
+                  <Button onClick={handleClick}>Submit</Button>
+
+                </ButtonGroup>
+              </Grid>
+            </Grid>
+          </Grid>
+          </Grid>
+
+      </label>
+    </>
+
+      )
+    }
+    
+    
 export default App
